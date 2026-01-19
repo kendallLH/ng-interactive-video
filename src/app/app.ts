5bc9292e2bf85e5ee84ccd9ffb42efd3d4c09be1
Kendall Haberer <kendallLH@users.noreply.github.com>
2026-01-19 16:00:45 -0500

    Add basic header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { Header } from './components/shared/header/header';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('ng-interactive-video');
